bb0a5ab39b2e8cf142f14cab56325c3931ab1c6e
Vinz Spring <contact@vinzspring.de>
2022-05-15 23:34:14 +0200

    build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
 export declare type Guard<T> = {
     [K in keyof T]: T[K] extends any ? T[K] extends never ? never : T[K] extends string ? StringConstructor : T[K] extends number ? NumberConstructor : T[K] extends boolean ? BooleanConstructor : T[K] extends Array<Guard<T[K]>> ? ArrayConstructor : T[K] extends object ? Guard<T[K]> : never : never;
 };
-export declare function check<T>(o: any, guard: Guard<T>): boolean;
+export declare function check<T>(o: any, guard: Guard<T>): o is T;
